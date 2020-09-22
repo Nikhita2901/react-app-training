dbb2f9eb2165a67450b62c09e367c881e9777ff4
Nikhita2901 <71366035+Nikhita2901@users.noreply.github.com>
2020-09-23 01:06:51 +0530

    Added ErrorBounday and Hero files Adding Error boundaries and where to add in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./styles.css";
 import PortalsDemo from "./Components/PortalsDemo";
+import Hero from "./Components/Hero";
+import ErrorBoundary from "./Components/ErrorBoundary";
 
 export default function App() {
   return (
     <div className="App">
-      <PortalsDemo />
+      {/* <PortalsDemo /> */}
+      <ErrorBoundary>
+        <Hero heroName="Batman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Superman" />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName="Joker" />
+      </ErrorBoundary>
     </div>
   );
 }
